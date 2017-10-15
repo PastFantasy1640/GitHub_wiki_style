12586c9970d890c4d627e58b27ae658dfe8c4c99
wgprojects2013 <pastfantasy1640@gmail.com>
2017-10-15 12:11:29 +0900

    関数化しました

--- test.php
+++ test.php
@@ -1,27 +1,27 @@
 <?php
 
-	$addr = $_GET['p'];
-	$str = file("https://github.com/PastFantasy1640/jubeon_pro/wiki/" . $addr);
-
-
-	$html_str = "";
-	$str_linenum = count($str);
-	$end_pattern = '/<\/div>/';
-	//362行目から、<div id="wiki-footer" class="wiki-footer">が出てくる直前まで
-	for($i = 362; $i < $str_linenum; $i++){
-		$html_str .= $str[$i] . "\n";
-		if(preg_match($end_pattern, $str[$i])) break;
-	}
-
+function getWikiText(){
+	//GET method
+	$user = $_GET['u'];	//username
+	$rep = $_GET['r'];		//repository name
+	//when GET['p'] is not set, home wiki is shown instead.
+	$addr = (isset($_GET['p']) ? $_GET['p'] : "");	//page id
 	
+	//result strings
+	$html_str = "";
+	if(isset($user) && isset($rep)){
+		$str = file("https://github.com/" . $user . "/" . $rep . "/" . $addr);
+		$str_linenum = count($str);
+		$end_pattern = '/<\/div>/';
+		//362行目から、<div id="wiki-footer" class="wiki-footer">が出てくる直前まで
+		for($i = 362; $i < $str_linenum; $i++){
+			$html_str .= $str[$i] . "\n";
+			if(preg_match($end_pattern, $str[$i])) break;
+		}
+	}
+	
+	return $html_str;
+}
+
 ?>
 
-<html>
-<head>
-</head>
-<body>
-<?php //var_dump($str); ?>
-<?php echo $html_str; ?>
-<?php //echo htmlspecialchars($str); ?>
-<body>
-</html>
