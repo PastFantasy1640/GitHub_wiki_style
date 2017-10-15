379cf61ea9ccf934a9a630055785312284f4e3e5
wgprojects2013 <pastfantasy1640@gmail.com>
2017-10-15 12:27:09 +0900

    関数の仕様を一部変更しました

--- getWikiText.php
+++ getWikiText.php
@@ -1,22 +1,16 @@
 <?php
 
-function getWikiText(){
-	//GET method
-	$user = $_GET['u'];	//username
-	$rep = $_GET['r'];		//repository name
-	//when GET['p'] is not set, home wiki is shown instead.
-	$addr = (isset($_GET['p']) ? $_GET['p'] : "");	//page id
-	
+function getWikiText($user_name, $repository_name, $page){
 	//result strings
 	$html_str = "";
 	if(isset($user) && isset($rep)){
 		$str = file("https://github.com/" . $user . "/" . $rep . "/" . $addr);
 		$str_linenum = count($str);
 		$end_pattern = '/<\/div>/';
-		//362行目から、<div id="wiki-footer" class="wiki-footer">が出てくる直前まで
-		for($i = 362; $i < $str_linenum; $i++){
-			$html_str .= $str[$i] . "\n";
+		//from 363line to the word : "</div>"
+		for($i = 363; $i < $str_linenum; $i++){
 			if(preg_match($end_pattern, $str[$i])) break;
+			$html_str .= $str[$i] . "\n";
 		}
 	}
 	
